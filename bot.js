432f5c14e3c3f26de610ba4ce469ac7034db9704
Maginet66 <38592268+Maginet66@users.noreply.github.com>
2018-04-21 23:17:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,22 @@
 
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+var prefix = '.';
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+	//.ping//
+  if (msg.content === prefix + 'ping') {
+    msg.reply('Pong!');
+  }
+  //.avatar//
+  if (msg.content === prefix + 'avatar') {
+	  msg.reply(msg.author.avatarURL);
+  }
+});
+
+client.login('process.env.BOT_TOKEN);
